73a08f09a49a36be5fce395d9cd221ad7344ba81
Chelsey Morris <chelseymorris91@yahoo.com>
2021-04-08 14:12:34 -0700

    added front end routes for execise and stats pages

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongojs = require("mongojs");
 const mongoose = require("mongoose");
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,7 +19,17 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// routes here
+//get route for /exercise
+
+app.get('/exercise', (req,res)=> {
+  res.sendFile(path.join(__dirname, 'public/exercise.html'))
+})
+
+app.get('/stats', (req,res)=> {
+  res.sendFile(path.join(__dirname, 'public/stats.html'))
+})
+
+//get route for /stats
 //get route for workouts /api/workouts gets all workouts
 
 app.get('/api/workouts', (req, res) => {
